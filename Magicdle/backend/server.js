32892e9d6478e626b38e11b8a6a910bee54850f2
Amka Baasanbat <141882871+amkanaut@users.noreply.github.com>
2026-05-01 16:16:21 -0600

    Updated MOngoDB URI with correct one
    Avoiding DNS connection with MongoDB. Had to use different string

--- Magicdle/backend/server.js
+++ Magicdle/backend/server.js
@@ -1,9 +1,9 @@
 // require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
-
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+
 
 
 const app = express();
